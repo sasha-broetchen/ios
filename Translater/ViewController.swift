0c9d173f7970b031d759b8e62ccfc7ebb7aa2dd5
sasha-broetchen <alexpampushko@gmail.com>
2018-03-28 15:10:34 +0300

    project added

---
+++ Translater/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  Translater
+//
+//  Created by Alex Pampushko on 25.03.2018.
+//  Copyright © 2018 Pampushko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let dict="account - счет, считать;act - действия;addition - дополнение;adjust - регулировать, приспосабливать;advertisement - реклама;agreement - соглашение;air - воздух, проветривать;amount - количество, означать, составлять;amusement - развлечение;animal - животное;answer - ответ, отвечать;apparatus - аппарат;approval - одобрение;argument - спор, аргумент;art - искусство, художественный;attack - нападение, нападать;attempt - попытка, пытаться;attention - внимание;attraction - достопримечательности;authority - власть, полномочие;back - назад, спина, отступать;balance - баланс, уравновешивать;base - основной;behavior - поведение;belief - вера;birth - рождение;bit - частица, бит;bite - укус, кусать;blood - кровь;blow - удар, дуть;body - тело;brass - медь, руководство;bread - хлеб;breath - дыхание;brother - брат;building - здание, строение;burn - ожог, гореть;burst - взрыв, взрывать;business - бизнес, деловой;butter - масло;canvas - холст;care - забота, лечение;cause - причина, вызывать;chalk - мел, рисовать мелом;chance - шанс, случайный, рисковать;change - изменение, замена;cloth - ткань, одежда;coal - уголь;color - цвет;comfort - комфорт;committee - комитет;company - компания;comparison - сравнение;competition - соревнование;condition - условие, обуславливать;connection - связь;control - контроль;cook - готовить;copper - медь;copy - копия;cork - пробка;cotton - хлопок;cough - кашель;country - страна;cover - покрытие, покрывать, обложка;crack - первоклассный, взламывать;credit - кредит, верить;crime - преступление;crush - давка, сокрушать;cry - плакать;current - текущий, поток;curve - кривая, избегать;damage - повреждение, повреждать;danger - опасность;daughter - дочь;day - день;death - смерть;debt - долг;decision - решение;degree - уровень, градус, степень;design - намереваться;desire - желание, желать;destruction - разрушение;detail - детализировать, детали;development - развитие;digestion - вываривание;direction - управление;discovery - открытие;discussion - обсуждение;disease - болезнь;disgust - отвращение;destruction - разрушение;detail - детализировать, детали;development - развитие;digestion - вываривание;direction - управление;discovery - открытие;discussion - обсуждение;disease - болезнь;disgust - отвращение;earth - земля;edge - продвигаться, край;education - образование;effect - производить, эффект;end - заканчивать, конец;error - ошибка;event - случай, событие;example - пример;exchange - обмен, обменивать;existence - существование;expansion - расширение;experience - опыт, испытывать;expert - опытный, эксперт;fact - факт;fall - падение, осень;family - семья;father - папа;fear - бояться, страх;feeling - чувство;fiction - беллетристика, фикция;field - область, поле, выставлять;fight - борьба, бороться;fire -огонь;flame - пылать, пламя;flight - полет, рейс;flower - цветок;fold - сгиб, сворачивать;food - еда, продовольствие;force - сила, вынуждать, вызывать;form - форма;friend - друг;front - выходить, фронт, передний;fruit - фрукт;glass - стекло, стакан;gold - золото;government - государство;grain - зерно;grass - трава;grip - власть, захват, захватывать;group - группа;growth - рост;guide - гид, справочник, путеводитель;harbor - приютить, питать, вставать на якорь, гавань;harmony - гармония;hate - ненавидеть, очень не хотеть, ненависть;hearing - слушание, слух;heat - нагревать, высокая температура;help - помощь, помогать;history - история;hole - продырявливать, отверстие;hope - надежда, надеяться;hour - час;humor - юмор;ice - лёд;idea - идея;impulse - импульс;increase - увеличивать, увеличение;industry - индустрия;ink - чернила;insect - насекомое;instrument - инструмент;insurance - страховка;interest - интерес, процент, доля;invention - изобретение;iron - утюг, гладить;jelly - превращать в желе, желе;join - присоединяться, объединение, соединение;journey - путешествовать, поездка;judge - судья, судить;jump - прыгать, прыжок;kick - удар, ударить;kiss - поцелуй, целовать;knowledge - знание;land - земля;language - язык;laugh - смеяться, смех;law - законный, закон;lead - принуждать, побеждать, вести, лидерство;learning - изучение, обучение;leather - кожа;letter - письмо;level - уровень, выравнивать, направлять;lift - поднимать, лифт;light - освещать, легкий, свет;limit - ограничивать;linen - льняной, полотно;liquid - жидкий, жидкость;list - список, перечислять;loss - потеря;love - любовь;machine - машина;man - человек;mark - марка;market - рынок;mass - масса;meal - еда;measure - мера, измерять;meat - мясо;meeting - встреча;memory - память;metal - металл;middle - средний, середина;milk - молоко;mind - ум, возражать;mine - мой, месторождение, мина;minute - минута;mist - туман;money - деньги;month - месяц;morning - утро;mother - мать;motion - двигаться, движение, жест;mountain - гора, горный;move - двигать, шаг, движение;music - музыка;name - имя;nation - нация;need - потребность, требоваться;news - новости;night - ночь;noise - шум;note - примечание, отмечать;number - число, номер;observation - наблюдение;offer - предложение, предлагать;oil - масло;operation - операция, действие;opinion - мнение;order - заказ, заказывать, приказывать;organization - организация;ornament - украшение, украшать;owner - владелец;page - страница;pain - боль, причинять боль;paint - краска, рисовать, красить;paper - бумага;part - часть, отделять, разделяться;paste - приклеивать, паста;payment - оплата;peace - мир;person - персона;place - размещать, помещать, занимать место, место;plant - завод, растение, прививать, сеять;play - играть;pleasure - удовольствие;point - пункт, точка, указывать;poison - яд, отравлять;polish - полировать;porter - швейцар,носильщик;position - помещать, позиция;powder - порошок;power - сила, власть;price - цена;print - печатать;process - обрабатывать, процесс;produce - продукт, производить;profit - прибыль, получать прибыльproperty - свойства;prose - проза;protest - возражать, протест;pull - напряжение, тянуть;punishment - наказание;purpose - намереваться, цель;push - толчок, подталкивать;quality - качество, качественный;question - вопрос;rain - дождь;range - диапазон, располагаться;rate - норма,разряд;ray - луч;reaction - реакция;reading - чтение;reason - рассуждать, причина, разум;record - рекордный, отчёт, делать запись;regret - сожаление, сожалеть;relation - отношение;religion - религия;representative - представитель;request - запрос, просить;respect - уважение, уважать;rest - отдых, отдыхать, оставаться;reward - награда, вознаграждать;rhythm - ритм;rice - рис;river - река;road - дорога;roll - рулон, ведомость, катиться, въезжать;and - и;or - или;room - комната;rub - протирать, тереться;rule - правило;run - бег, бегать;salt - соль, солить;sand - песок;scale - измерять, масштаб;science - наука;sea - море;seat - сиденье, усаживать, место;secretary - секретарь;selection - выбор;self - сам;sense - чувство, значения, смысл, ощущать;servant - слуга;sex - секс, пол;shade - оттенок,тень, заштриховывать;shake - встряска, встряхивать, дрожать, потрясать;shame - позор, позорить;shock - шок, потрясать;side - сторона, примыкать;sign - знак, признак, подписывать;silk - шёлк;silver - серебро;sister - сестра;size - размер;sky - небо;sleep - спать;slip - промах, бланк, подсовывать, скользить;slope - наклон, клониться;smash - удар, разбиваться;smell - запах, пахнуть;smile - улыбка, улыбаться;smoke - дым, курить;sneeze - чиханье, чихать;snow - снег;soap - мыло, мылить;society - общество;son - сын;song - песня;sort - вид, сортировать;sound - звук;soup - суп;space - пространство, космос;stage - стадия, сцена, организовывать;start - начинать;statement - утверждение;steam - пар, париться, двигаться;steel - сталь;step - шаг, шагать;stitch - стежок, сшивать;stone - камень;stop - останавливаться, остановка;story - история;stretch - отрезки, протягивать, простираться;structure - структура;substance - вещество, сущность;sugar - сахар;suggestion - предложение, предположение;summer - лето;support - поддержка, поддерживать;surprise - сюрприз;swim - плаванье, плавать;system - система;talk - разговор, говорить;taste - вкус, испытывать;tax - налог, облагать налогом;teaching - обучение;tendency - тенденция;test - проверять, тест;theory - теория;thing - вещь;thought - мысль;thunder - гром, греметь;time - время;tin - олово,консервная банка;top - возглавлять, вершина, главный;touch - прикосновение, касаться;trade - торговля, обменивать;transport  - транспорт, транспортировать;trick - уловка, обманывать;trouble - проблема, беспокоить;turn - поворот, поворачивать;twist - завихрение, крутить;unit - единица;use - использование, использовать;value - ценность, оценивать;verse - стих;vessel - судно, сосуд;in - в;which - который;view - вид, взгляд, рассматривать;voice - голос, высказывать;war - война;wash - мытьё, стирать, чистить;waste - ненужный, отходы, трата;water - вода;wave - волна;wax - воск, натирать воском;weather - погода;week - неделя;weight - вес, нагружать;wind - ветер;wine - вино;winter - зима, зимовать;woman - женщина;wood - лес;wool - шерсть;word - слово;work - работа, работать;wound - проветривать, рана, ранить;writing - письмо;year - год;i - я;you - ты;he - он;she - она;it - оно;they - они;we - мы"
+    let fileManager = FileManager();
+    let tempDir = NSTemporaryDirectory();
+    let fileName:String = "dict.txt";
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let directoryWithFiles=checkDirectory() ?? "Empty";
+        text.text = "They work in company which produce wax and wool"
+        //translateTetxt.text = directoryWithFiles;
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func checkDirectory()->String?
+    {
+        do
+        {
+            let filesInDirectory = try fileManager.contentsOfDirectory(atPath: tempDir)
+            
+            let files = filesInDirectory
+            if files.count > 0 {
+                if files.first == fileName {
+                    print("dict.txt found")
+                    return files.first
+                } else {
+                    print("File not found")
+                    return nil;
+                }
+            }
+        } catch let error as NSError {
+            print(error);
+        }
+        return nil;
+    }
+    func findWord( word: String )->String?
+    {
+        for sentence in dict.components(separatedBy: ";")
+        {
+            let t=sentence.components(separatedBy: " -");
+            if(t[0]==word)
+            {
+                return t[1];
+            }
+            
+            
+        }
+        return " ";
+    }
+    @IBAction func tarnslateButton(_ sender: Any) {
+        var trn :String = "";
+        
+        view.endEditing(true);
+        let text1:String = text.text!;
+        let stringOfWordsArray=text1.components(separatedBy:" ");
+        for word in stringOfWordsArray{
+            trn = trn + word + " - " + findWord(word: word.lowercased())!+"\n" ;
+            
+        };
+        translateTetxt.text = trn;
+        text.text="";
+    }
+    
+    @IBOutlet var text: UITextField!
+    
+    @IBOutlet var translateTetxt: UITextView!
+}
+
